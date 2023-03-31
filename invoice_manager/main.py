59fb69aefdbebeae4698b007624233123d592bc3
Pablo RP <pablorodriper@gmail.com>
2023-03-31 09:26:04 +0000

    First POC

---
+++ invoice_manager/main.py
@@ -0,0 +1,67 @@
+import pdfplumber
+import regex as re
+from glob import glob
+
+def search_regex(text, regex):
+    if found_text := re.search(regex, text):
+        print(found_text.group(0))
+        return found_text.group(0)
+
+    return None
+
+results = []
+
+for pdf_path in glob("../data/*.pdf"):
+    print(pdf_path)
+
+    with pdfplumber.open(pdf_path) as pdf:
+
+        first_page = pdf.pages[0]
+        first_page_text = first_page.extract_text(x_tolerance=3, y_tolerance=3, layout=False, x_density=7.25, y_density=13)
+        
+        second_page = pdf.pages[1]
+        x0 = second_page.search("Potencia facturada", regex=True)[0]["x0"]
+        top = second_page.search("Potencia facturada", regex=True)[0]["top"]
+        bottom = second_page.search("TOTAL IMPORTE FACTURA", regex=True)[0]["bottom"]
+        second_page_text = second_page.crop((x0, top, second_page.width, bottom)) \
+                            .extract_text(x_tolerance=3, y_tolerance=3, layout=False, x_density=7.25, y_density=13)
+
+        invoice_data = {
+            "Fecha": None,
+            "Potencia facturada": None,
+            "€ Potencia punta": None,
+            "€ Potencia valle": None,
+            "Energía facturada": None,
+            "€ Energía facturada": None,
+            "€ Bono social": None,
+            "€ Tope precio del gas": None,
+            "€ Mecanismo ajuste": None,
+            "€ Alquiler equipos medida": None,
+            "TOTAL IMPORTE FACTURA": None,
+        }
+
+        invoice_data["Fecha"] = search_regex(first_page_text, r"\d\d/\d\d/\d\d\d\d")
+
+        invoice_data["Potencia facturada"] = search_regex(second_page_text, r"(?<=Potencia facturada.*)(\d,\d+)(?= kW)")
+        invoice_data["€ Potencia punta"] = search_regex(second_page_text, r"(?<=Punta.*x )(-?0,\d+)(?=\s./kW)")
+        invoice_data["€ Potencia valle"] = search_regex(second_page_text, r"(?<=Valle.*x )(-?0,\d+)(?=\s./kW)")
+        invoice_data["Energía facturada"] = search_regex(second_page_text, r"(?<=Energía facturada.*)(-?\d+)(?= kW)")
+        invoice_data["€ Energía facturada"] = search_regex(second_page_text, r"(?<=Energía facturada.*x )(-?0,\d+)(?= ./kW)")
+        invoice_data["€ Bono social"] = search_regex(second_page_text, r"(?<=bono social.*x )(-?0,\d+)(?= ./día)")
+        invoice_data["€ Tope precio del gas"] = search_regex(second_page_text, r"(?<=Tope precio del gas.*x )(-?0,\d+)(?= ./kW)")
+        invoice_data["€ Mecanismo ajuste"] = search_regex(second_page_text, r"(?<=Mecanismo ajuste.*x )(-?0,\d+)(?= ./kW)")
+        invoice_data["€ Alquiler equipos medida"] = search_regex(second_page_text, r"(?<=Alquiler equipos medida.*x )(-?0,\d+)(?= ./día)")
+        invoice_data["TOTAL IMPORTE FACTURA"] = search_regex(second_page_text, r"(?<=TOTAL IMPORTE FACTURA )(-?\d+,\d+)")
+
+        print(invoice_data)
+
+        results.append(invoice_data)
+
+
+import pandas as pd
+            
+df = pd.DataFrame(results)
+df["Fecha"] = pd.to_datetime(df["Fecha"], format="%d/%m/%Y")
+df = df.sort_values(by="Fecha")
+
+print(df)
